e0782808180e4eb607ef3e1af1cc58e15fa9941d
Mr_Root <61718775+didwmsabargayary@users.noreply.github.com>
2025-06-17 13:03:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ const menuItems = [
     { id: 5, name: 'Cigarette (चिगरेट)', price: 10, image: 'ch.jpg' },
     // { id: 8, name: 'Roti (रोटी)', price: 10, image: 'roti.webp' },         
     { id: 11, name: 'Pan (गय पाथै)', price: 5, image: 'goy.jpeg' },
-    { id: 15, name: 'Imli Kick', price: 1, image: 'em.jpeg' }
+    { id: 15, name: 'Imli Kick', price: 1, image: 'em.jpeg' },
+    { id: 17, name: 'MAAZA', price: 1, image: 'maaza.jpeg' },
+    { id: 18, name: 'Fanta', price: 1, image: 'fanta.jpeg' }
     //{ id: 12, name: 'Puri (पुरी)', price: 10, image: 'puri.jpg' },
    // { id: 13, name: 'Sugar Cane Juice (खुसेर बिदै)', price: 20, image: 'suger.jpg' }
    // { id: 14, name: 'Chow Mein (चाउ मेन)', price: 60, image: 'R.jpg' },
